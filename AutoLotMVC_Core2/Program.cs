8c908ffa9a83569e9efbf9903a1ad868f72536fc
unknown <Administrator@Eshref-Se.bestcomp.local>
2018-01-26 17:28:33 +0400

    Add project files.

---
+++ AutoLotMVC_Core2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using AutoLotDAL_Core2.DataInitialization;
+using AutoLotDAL_Core2.EF;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutoLotMVC_Core2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = BuildWebHost(args);
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<AutoLotContext>();
+                //MyDataInitializer.RecreateDatabase(context);
+                //MyDataInitializer.InitializeData(context);
+            }
+            webHost.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
